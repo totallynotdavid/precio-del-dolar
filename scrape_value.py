d05baae849400d2db0e01c2ce6a8f143786e564e
David <dadch1404@gmail.com>
2023-02-16 07:58:06 +0000

    feat: write the results as a json file - compra - venta - imagen

--- scrape_value.py
+++ scrape_value.py
@@ -3,6 +3,7 @@ Author: totallynotdavid
 Date: 16/02/2023
 """
 
+import json
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,20 +12,31 @@ response = requests.get(URL, timeout=5)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 first_div = soup.find('div', {'class': 'mx-1 md:mx-3'})
-
 hidden_divs = first_div.find_all('div', {'class': 'hidden md:block'}) # type: ignore
-result = {}
 
+result = []
 for i, hidden_div in enumerate(hidden_divs):
     exchange_div = hidden_div.find('div', {'class': 'exchange_div flex justify-center'})
-    values = []
-    p_elements = exchange_div.find_all('p', {'class': 'ValueQuotation_text___mR_0'})
-    for p in p_elements:
-        values.append(p.text)
-    result[f"{i}"] = values
+    exchange_img = hidden_div.find_all('img')
 
-for group, values in result.items():
-    print(f"{group}: ")
-    for i, value in enumerate(values):
-        LABEL = "Compra" if i == 0 else "Venta"
-        print(f"{LABEL}: {value}")
+    if exchange_div and exchange_img:
+        values = []
+
+        p_elements = exchange_div.find_all('p', {'class': 'ValueQuotation_text___mR_0'})
+        for p in p_elements:
+            values.append(p.text)
+
+        for img in exchange_img:
+            img_src = img.get('src', '')
+
+        result.append ({
+            "Compra": values[0],
+            "Venta": values[1],
+            "ImageSrc": img_src
+        })
+
+# Write the results to a file
+with open('results.json', 'w', encoding='utf-8') as f:
+    json.dump(result, f, indent=4)
+
+print("Results written to file 'results.json'")
