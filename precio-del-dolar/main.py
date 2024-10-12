80143ab87def4188e06f5af1e3ecc491273b46e7
David <dadch1404@gmail.com>
2024-10-12 04:36:25 +0000

    chore: add ruff for linting and code formatting; remove unused scripts

--- precio-del-dolar/main.py
+++ precio-del-dolar/main.py
@@ -1,22 +1,15 @@
-"""
-Author: totallynotdavid
-Date: 16/02/2023
-"""
-
 import json
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, how exactly did you get here?</p>"
 
 @app.route('/minimum_price')
 def minimum_price():
     with open('minimum_price.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
     return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run()
